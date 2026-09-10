8c4b4551ce1e6fa9a4362f3bf763fcbefd0def6e
a.kain <a.kain@vktech.ru>
2026-09-10 18:25:02 +0300

    add resistance calc

---
+++ README.md
@@ -0,0 +1,6 @@
+# Potentiometer resistance measurement
+
+## Potentiometer allows to change the resistance value in the curcuit by moving the knob
+
+## Given 5v input  it will have 1023 ADC value (ticks) (640 if given 3.3V)
+## By dividing the value by 1023 we can calculate the resistance. in my case 10000 ohms
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,28 @@
 #include <Arduino.h>
 
+const float totalResistance = 10000;
+const float maxADCValue = 1023;
+
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // initialize serial communication at 9600 bits per second
 }
 
 void loop() {
-  int sensorValue = analogRead(A0);
-  Serial.println(sensorValue);
-  delay(10);
+  int sensorValue = analogRead(A0); // read the input on analog pin A0
+  
+  if (sensorValue > maxADCValue) {
+    sensorValue = maxADCValue;
+  }
+
+  float curResistance = sensorValue / maxADCValue * totalResistance;
+
+  Serial.print("ADC: ");
+  Serial.print(sensorValue);
+  Serial.print(" | Resistance: ");
+  Serial.print(curResistance, 1); // 1 decimal place
+  Serial.println(" Ohm");
+
+
+  delay(10); // wait 10 milliseconds before the next reading
 }
